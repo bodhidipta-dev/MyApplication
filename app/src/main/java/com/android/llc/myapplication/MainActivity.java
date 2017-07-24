be6b066fff594ba126e4b5fb01e8760141af65d1
Bodhidipta <bodhidiptabhattacharjee@gmail.com>
2017-07-24 14:41:50 +0530

    Camera Interated

--- app/src/main/java/com/android/llc/myapplication/MainActivity.java
+++ app/src/main/java/com/android/llc/myapplication/MainActivity.java
@@ -1,9 +1,16 @@
 package com.android.llc.myapplication;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
@@ -22,10 +29,17 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.llc.myapplication.adapter.VerificationOptionModeAdapter;
+import com.android.llc.myapplication.utils.ImageTakerActivityCamera;
+import com.android.llc.myapplication.utils.PermissionController;
+import com.android.llc.myapplication.utils.ProConstant;
 import com.android.llc.myapplication.viewmod.CropCircleTransformation;
 import com.android.llc.myapplication.viewmod.NavigationHandler;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -36,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> option_mode;
     private VerificationOptionModeAdapter adapter;
     private ImageView optionImage;
+    private static final int REQUEST_IMAGE_CAPTURE = 5;
+    private static final int PICK_IMAGE = 3;
+    private String mCurrentPhotoPath = "";
+    private ImageView back_image, nav_camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +62,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
+        nav_camera = (ImageView) findViewById(R.id.nav_camera);
+
+        back_image = (ImageView) findViewById(R.id.back_image);
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        nav_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PermissionController.class);
+                intent.setAction(PermissionController.ACTION_READ_STORAGE_PERMISSION);
+                startActivityForResult(intent, 200);
+            }
+        });
 
         findViewById(R.id.drawer_toggle).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -158,4 +187,98 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
     }
+
+    private void showImagePickerOption() {
+        new AlertDialog.Builder(MainActivity.this)
+                .setCancelable(true)
+                .setTitle("Background image")
+                .setMessage("please choose image source type.")
+                .setPositiveButton("Gallery", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        Intent intent = new Intent(Intent.ACTION_PICK);
+                        if (intent.resolveActivity(getPackageManager()) != null) {
+                            intent.setType("image/*");
+                            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+
+                        }
+                    }
+                })
+                .setNegativeButton("Camera", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        ProConstant.cameraRequested = true;
+                        startActivityForResult(new Intent(MainActivity.this, ImageTakerActivityCamera.class), REQUEST_IMAGE_CAPTURE);
+                    }
+                })
+                .create()
+                .show();
+    }
+
+    @Override
+    public void onActivityResult(final int requestCode, int resultCode, final Intent data) {
+        try {
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (data != null) {
+                            mCurrentPhotoPath = data.getExtras().get("data").toString();
+                            Glide.with(MainActivity.this).load("file://" + mCurrentPhotoPath).into(new GlideDrawableImageViewTarget(back_image) {
+                                @Override
+                                public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
+                                    super.onResourceReady(resource, animation);
+                                }
+                            });
+                        }
+                    }
+                }, 800);
+            } else if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+                try {
+                    Uri uri = data.getData();
+                    File dataFile = new File(getRealPathFromURI(uri));
+                    if (!dataFile.exists())
+                        mCurrentPhotoPath = dataFile.getAbsolutePath();
+                    Glide.with(MainActivity.this).load(uri).fitCenter().into(new GlideDrawableImageViewTarget(back_image) {
+                        /**
+                         * {@inheritDoc}
+                         * If no {@link GlideAnimation} is given or if the animation does not set the
+                         * {@link Drawable} on the view, the drawable is set using
+                         * {@link ImageView#setImageDrawable(Drawable)}.
+                         *
+                         * @param resource  {@inheritDoc}
+                         * @param animation {@inheritDoc}
+                         */
+                        @Override
+                        public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
+                            super.onResourceReady(resource, animation);
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            } else if (requestCode == 200 && resultCode == RESULT_OK) {
+                showImagePickerOption();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getRealPathFromURI(Uri contentURI) {
+        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) {
+            // Source is Dropbox or other similar local file path
+            return contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(idx);
+        }
+    }
+
 }
